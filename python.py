3f77034a3b4296a7ca2f208b446d1a0997a8b132
Ryota Konabe <rt.konabe@gmail.com>
2020-11-09 00:01:35 +0900

    素数リストの追加

--- python.py
+++ python.py
@@ -1,6 +1,8 @@
+import math
+
 # 1行に空白区切りの数字
-#a = [int(ch) for ch in input().split()]  # 1行に空白区切りの数字
+#a = [int(ch) for ch in input().split() ]
 # N行に空白区切りの数字
 #a = [ [int(ch) for ch in input().split()] for i in range(N) ]
-
-
+# 素数リスト (import mathすること)
+# primes = [x for x in range(2, 1000) if all([x%y != 0 for y in range(2, math.floor(math.sqrt(x)))])]
